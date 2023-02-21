9b98bf77af3b3aae2409dc18323bb6e9a5582c74
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-21 12:40:40 +0300

    vk-parse-done

--- src/py-parse.py
+++ src/py-parse.py
@@ -1,8 +1,23 @@
 from vk_api import VkApi
 from vk_api.utils import get_random_id
+from secrets import API_KEY
+import json
 
-ACCESS_TOKEN = ...  # Подставьте свой
-PEER_ID = ...       # Подставьте свой
+ACCESS_TOKEN = API_KEY
 
 vkSession = VkApi(token=ACCESS_TOKEN)
-vk = vkSession.get_api()
+api = vkSession.get_api()
+
+posts = api.wall.get(
+    domain='silaedr',
+)
+
+postsDict = {}
+for i in range(0, 5):
+    if (posts['items'][i]['text'][0:116].count("\n") > 0):
+        postsDict.update({str(i): posts['items'][i]['text'][0:posts['items'][i]['text'].find("\n")]})
+    else:
+        postsDict.update({str(i): posts['items'][i]['text'][0:116]+"..."})
+
+with open("public/posts.json", "w") as f:
+    json.dump(postsDict, f, ensure_ascii=False)
