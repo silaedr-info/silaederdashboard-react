dfd038f27b89afd6618ce26285a2ae71ff1f28fc
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-22 10:42:51 +0300

    navigation system

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { SideBar, SideBarLink } from './Components'
 import { IoMdNotifications } from 'react-icons/io'
 import { Home } from './Home';
+import { useState } from 'react';
+import { AutoDiploma } from './AutoDiploma';
 
 function App() {
+    const [active, setActive] = useState([true, false, false, false, false]);
+
+    function ChNav(n) {
+        var tmp = [false, false, false, false, false]
+        tmp[n] = true;
+        setActive(tmp);
+    }
+
     return (
         <div className='w-full h-screen'>
             <nav className='flex w-full justify-between p-4 fixed top-0 items-center z-10 backdrop-blur-md'>
@@ -15,14 +25,21 @@ function App() {
             <div className="pt-20 h-full flex w-full">
                 <SideBar className="fixed" content={
                     <div>
-                        <SideBarLink text="Главная" active={true} />
-                        <SideBarLink text="Silaeder Server" className="mt-5" active={false} />
-                        <SideBarLink text="Silaeder Conference" className="mt-5" active={false} />
-                        <SideBarLink text="Генератор дипломов" className="mt-5" active={false} />
-                        <SideBarLink text="Оценки и расписание" className="mt-5" active={false} />
+                        <SideBarLink text="Главная" active={active[0]} onClick={() => { ChNav(0) }} />
+                        <SideBarLink text="Silaeder Server" className="mt-5" onClick={() => { ChNav(1) }} active={active[1]} />
+                        <SideBarLink text="Silaeder Conference" className="mt-5" onClick={() => { ChNav(2) }} active={active[2]} />
+                        <SideBarLink text="Генератор дипломов" className="mt-5" onClick={() => { ChNav(3) }} active={active[3]} />
+                        <SideBarLink text="Оценки и расписание" className="mt-5" onClick={() => { ChNav(4) }} active={active[4]} />
                     </div>
                 } />
-                <Home className=" w-7/12 sm:w-4/5 absolute right-4" />
+
+                {active[0] &&
+                    <Home className=" w-7/12 sm:w-4/5 absolute right-4" />
+                }
+
+                {active[3] &&
+                    <AutoDiploma className=" w-7/12 sm:w-4/5 absolute right-4" />
+                }
             </div>
         </div>
     )
