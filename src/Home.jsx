7f110e1db57e4016041d8b3035f4a7a1a0de8297
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-22 10:02:30 +0300

    fixed bugs

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
-import { Card, CardCustom, CardList, CardListItem } from "./Components"
+import { Card, CardCustom } from "./Components"
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { News } from "./VkNews";
 
 function Home(props) {
     ChartJS.register(ArcElement, Tooltip, Legend);
@@ -39,16 +40,7 @@ function Home(props) {
                 <CardCustom className="text-center" name="Средняя оценка за неделю" desc={<Doughnut data={data} options={options} width="200" />} />
             </div>
 
-            <CardList className="mt-5 mb-10" name="Новости" desc={
-                <div>
-                    <CardListItem contents={<a target="_blank" rel="noreferrer" className="underline transition-all ease-in-out duration-500 hover:text-prelg" href="https://vk.com/silaedr?w=wall-144869743_3681">В рамках уроков «Разговоры о важном» лицей НИУ ВШЭ пригласил наших 11-классниц Дашу и Наташу рассказать о своем проекте</a>} />
-                    <CardListItem contents={<a target="_blank" rel="noreferrer" className="underline transition-all ease-in-out duration-500 hover:text-prelg" href="https://vk.com/silaedr?w=wall-144869743_3680">5 и 6 классы Силаэдра посетили Царь-макет на ВДНХ Экскурсия была увлекательной. Всем очень понравилось.</a>} />
-                    <CardListItem contents={<a target="_blank" rel="noreferrer" className="underline transition-all ease-in-out duration-500 hover:text-prelg" href="https://vk.com/silaedr?w=wall-144869743_3678">Мастер-класс руководителя it-отдела компании Встреча с Игорем Котляром, руководителем IT- департамента компании BUDU "Ренессанс Страхование"</a>} />
-                    <CardListItem contents={<a target="_blank" rel="noreferrer" className="underline transition-all ease-in-out duration-500 hover:text-prelg" href="https://vk.com/silaedr?w=wall-144869743_3676">5 и 6 классы обсуждают компьютерную безопасность и цифровую гигиену.
-                        В этот раз по впечатлениям Никиты и Льва после прочтения книги Кевина Митника «Искусство быть невидимым»</a>} />
-                    <CardListItem contents={<a target="_blank" rel="noreferrer" className="underline transition-all ease-in-out duration-500 hover:text-prelg" href="https://vk.com/silaedr?w=wall-144869743_3670">Еще несколько слов о Конференции Силаэдр. Статистика участников и победителей конкурса по годам.</a>} />
-                </div>
-            } />
+            <News />
         </div>
     )
 }
