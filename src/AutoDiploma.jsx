6be84b240b283b572bd542901348ff434a8b8c52
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-23 12:14:40 +0300

    finished autodiploma design

--- src/AutoDiploma.jsx
+++ src/AutoDiploma.jsx
@@ -1,22 +1,55 @@
 import { useState } from "react"
-import { Input, ButtonSubmit } from "./Components"
+import { Input, ButtonSubmit, InputFile, Tab } from "./Components"
 
 function AutoDiploma(props) {
-    const [isCsv, setIsCsv] = useState(false)
+    const [isCsv, setIsCsv] = useState(false);
+
+    function checked(isChecked) {
+        setIsCsv(isChecked);
+    }
 
     return (
-        <div className={props.className + " ml-3"}>
-            <form action="/api/upload" className="flex flex-col items-center">
-                <span className="text-xl font-light">Шаблон: </span> <Input type="text" name="template" className="block mb-5" />
-                <span className="text-xl font-light">Количество дополнительных полей: </span> <Input type="number" name="have_project" className="block mb-5" />
-                <span className="text-xl font-light">ФИО человека: </span> <Input type="text" name="name" className="block mb-5" />
-                <span className="text-xl font-light">Название проекта: </span> <Input type="text" name="project" className="block mb-5" />
+        <main className={props.className + " flex flex-col"}>
+            <div className={"ml-3 flex justify-center"}>
+                {isCsv ?
+                    <form action="/api/upload" className="flex flex-col items-center mr-12">
+                        <div className=" text-center mb-5">
+                            <Tab text="Форма" className="rounded-l-lg" onClick={() => { checked(!isCsv) }} selected={!isCsv} />
+                            <Tab text="Csv файл" className="rounded-r-lg" onClick={() => { checked(isCsv) }} selected={isCsv} />
+                        </div>
+                        <div className="flex items-center flex-col mb-14 mt-12">
+                            <span className="text-xl font-light">Csv файл должен быть вида (разделитель - `):</span>
+                            <table className="border-solid border-2 rounded-lg border-separate border-spacing-0 border-black mt-2 ">
+                                <thead className="border-solid">
+                                    <tr>
+                                        <th className="border-solid border-gray-400 border-r-2 py-3">Название диплома</th>
+                                        <th className="border-solid border-gray-400 border-r-2">Количество дополнительных полей</th>
+                                        <th className="border-solid border-gray-400 border-r-2">ФИО человека</th>
+                                        <th>Название проекта</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                        <span className="text-xl font-light">Файл: </span> <InputFile className="block mb-5" />
 
-                <span className="text-xl font-light">Файл: </span> <Input type="file" className="block mb-5" />
+                        <ButtonSubmit text="Создать" />
+                    </form>
+                    :
+                    <form action="/api/upload" className="flex flex-col items-center mr-32">
+                        <div className=" text-center mb-5">
+                            <Tab text="Форма" className="rounded-l-lg" onClick={() => { checked(isCsv) }} selected={!isCsv} />
+                            <Tab text="Csv файл" className="rounded-r-lg" onClick={() => { checked(!isCsv) }} selected={isCsv} />
+                        </div>
+                        <span className="text-xl font-light">Шаблон: </span> <Input type="text" name="template" className="block mb-10 w-full" />
+                        <span className="text-xl font-light">Количество дополнительных полей: </span> <Input type="number" name="have_project" className="block mb-10 w-full" />
+                        <span className="text-xl font-light">ФИО человека: </span> <Input type="text" name="name" className="block mb-10 w-full" />
+                        <span className="text-xl font-light">Название проекта: </span> <Input type="text" name="project" className="block mb-5 w-full" />
 
-                <ButtonSubmit text="Создать" />
-            </form>
-        </div>
+                        <ButtonSubmit text="Создать" />
+                    </form>}
+                <img src="/ex.png" alt="diploma" className="w-[35%] rounded-md" />
+            </div>
+        </main>
     )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -36,7 +36,7 @@ function Home(props) {
     return (
         <div className={props.className + " ml-3"}>
             <div className="flex items-center flex-col sm:flex-row">
-                <Card className="mr-0 mb-5 sm:mr-5 sm:mb-0" name="Сводка" img_src="task.png" desc={<span>Оценки по предметам <strong>Алгебра, Русский язык</strong> стали лучше. На следующей неделе разбор листочка <strong>Вероятность</strong>. <strong>Поторопитесь</strong>, у Вас всё еще 0 баллов!</span>} />
+                <Card className="mr-0 mb-5 sm:mr-5 sm:mb-0" name="Сводка" img_src="/task.png" desc={<span>Оценки по предметам <strong>Алгебра, Русский язык</strong> стали лучше. На следующей неделе разбор листочка <strong>Вероятность</strong>. <strong>Поторопитесь</strong>, у Вас всё еще 0 баллов!</span>} />
                 <CardCustom className="text-center" name="Средняя оценка за неделю" desc={<Doughnut data={data} options={options} width="200" />} />
             </div>
 
